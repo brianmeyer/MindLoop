1f2ae34a0bb9633ffea4f3c34409efee581c4238
brianmeyer <68919921+brianmeyer@users.noreply.github.com>
2025-10-26 13:09:45 -0400

    Initial Commit

---
+++ MindLoop/MindLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindLoopApp.swift
+//  MindLoop
+//
+//  Created by Brian Meyer on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MindLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MindLoopTests/MindLoopTests.swift
@@ -0,0 +1,17 @@
+//
+//  MindLoopTests.swift
+//  MindLoopTests
+//
+//  Created by Brian Meyer on 10/26/25.
+//
+
+import Testing
+@testable import MindLoop
+
+struct MindLoopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
